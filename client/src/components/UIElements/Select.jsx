cedb0b244245117cff99677b54b6ddb661fb7d8c
jelenkoo10 <veljkojelenkovic00@gmail.com>
2023-05-12 16:45:14 +0200

    Adding branches select on signup

---
+++ client/src/components/UIElements/Select.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Select = (props) => {
+  const { options, selectName, selectId, selectStyle, labelName, labelStyle } =
+    props;
+
+  return (
+    <div className="flex justify-between items-center">
+      <label className={labelStyle} htmlFor={selectId}>
+        {labelName}
+      </label>
+      <select className={selectStyle} name={selectName} id={selectId}>
+        {options.map(({ id, name }) => {
+          return (
+            <option value={id} key={id}>
+              {name}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import Button from "../components/UIElements/Button";
 import Input from "../components/UIElements/Input";
+import Select from "../components/UIElements/Select";
+import { useHttpClient } from "../hooks/http-hook";
 
 const Signup = () => {
+  const [branches, setBranches] = useState([]);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    async function getBranches() {
+      const responseData = await sendRequest(
+        "http://localhost:5000/api/branches",
+        "GET",
+        null,
+        { "Content-Type": "application/json" }
+      );
+      setBranches(
+        responseData.branches.map((branch) => {
+          return {
+            name: branch.name + ", " + branch.city,
+            id: branch.id,
+          };
+        })
+      );
+    }
+
+    getBranches();
+  }, []);
+
   return (
     <form className="px-10 py-20 mx-auto bg-white w-1/3 opacity-80 mt-[60px] rounded-3xl">
       <Input
@@ -45,13 +72,13 @@ const Signup = () => {
         inputType="password"
         inputLabel="Šifra "
       />
-      <Input
-        inputId="branchId"
-        inputStyle="my-4 block border-b-2 border-[#B8572A] focus:border w-[200px]"
-        divStyle="flex justify-between items-center"
+      <Select
+        selectStyle="my-4 block border-b-2 border-[#B8572A] w-[200px]"
+        selectId="branch"
+        selectName="branch"
+        labelName="Ogranak"
         labelStyle="text-2xl text-[#C75D2C]"
-        inputType="text"
-        inputLabel="Ogranak "
+        options={branches}
       />
       <Button
         btnStyle="mx-auto mt-10 block bg-[#C75D2C] px-6 py-2 text-white text-lg font-bold rounded-md hover:bg-[#D76D3C]"

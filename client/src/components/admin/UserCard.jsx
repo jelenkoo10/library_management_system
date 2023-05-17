e6b09307f3368a24aead1fad167e5f6da9bdd4a9
Kila369 <67660152+Kila369@users.noreply.github.com>
2023-05-17 09:30:55 +0200

    assign

--- client/src/components/admin/UserCard.jsx
+++ client/src/components/admin/UserCard.jsx
@@ -1,41 +1,60 @@
-import { useContext, useEffect } from "react";
-import { AuthContext } from "../../context/auth-context";
+import { useEffect, useState } from "react";
 import Button from "../UIElements/Button";
+import { useHttpClient } from "../../hooks/http-hook";
 
 const UserCard = (props) => {
-  const { user, cardStyle, deleteBook, updateBook } = props;
-  const auth = useContext(AuthContext);
-  const userData = auth.isLoggedIn
-    ? JSON.parse(localStorage.getItem("userData")).is_admin
-    : null;
+  const { user, cardStyle, assignBook, returnBook } = props;
+  const [branches, setBranches] = useState([]);
+  const { sendRequest } = useHttpClient();
 
-  const handleDelete = async () => {
-    deleteBook(user.id);
+  const handleReturn = async () => {
+    returnBook(user);
   };
 
-  const handleUpdate = async () => {
-    updateBook(user);
+  const handleAssign = async () => {
+    assignBook(user);
   };
+
+  useEffect(() => {
+    async function fetchBranches() {
+      const responseData = await sendRequest(
+        `http://localhost:5000/api/users/branches/${user.id}`,
+        "GET",
+        null,
+        {
+          "Content-Type": "application/json",
+        }
+      );
+      setBranches(responseData.branches);
+    }
+    fetchBranches();
+  }, []);
 
   return (
     <div className={cardStyle}>
       <h1 className="text-xl font-bold">{user.name + "" + user.surname}</h1>
       <p className="text-md font-semibold">{user.email}</p>
-      <p className="text-sm">Rok isteka pozajmice: {user.phone}</p>
+      <p className="text-sm">Telefon: {user.phone}</p>
 
-      {/* {book.branchName && (
-            <p className="text-sm">Ogranak: {book.branchName}</p>
-          )} */}
+      {branches && (
+        <p className="text-sm">
+          Ogranci:
+          {branches.map((branch) => {
+            const coma = branches.lenght > 1 ? ", " : "";
+            return branch.name + coma;
+          })}
+        </p>
+      )}
       <div>
         <Button
           btnStyle="p-2 mx-2 bg-white text-[#C75D2C] font-semibold opacity-100 rounded-md"
-          btnText="Izmeni"
-          onClick={handleUpdate}
+          btnText="Upisi"
+          onClick={handleAssign}
         />
         <Button
           btnStyle="p-2 mx-2 mt-3 bg-[#C75D2C] text-white font-semibold opacity-100 rounded-md"
-          btnText="Obriši"
-          onClick={handleDelete}
+          btnText="Vrati"
+          onClick={handleReturn}
         />
       </div>
     </div>
--- client/src/components/admin/Users.jsx
+++ client/src/components/admin/Users.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { useHttpClient } from "../../hooks/http-hook";
 import UserCard from "./UserCard";
+import UserAssignModal from "./UserAssignModal";
 
 const Users = (props) => {
   const [users, setUsers] = useState([]);
-  const { error, sendRequest } = useHttpClient();
+  const { sendRequest } = useHttpClient();
+  const [assign, setAssign] = useState(null);
+  const [returnBookFromUser, setReturnBookFromUser] = useState(null);
 
   useEffect(() => {
     async function fetchUsers() {
@@ -20,6 +23,20 @@ const Users = (props) => {
     }
     fetchUsers();
   }, []);
+
+  const assignBook = (user) => {
+    setAssign(user);
+  };
+
+  const returnBook = (user) => {
+    setReturnBookFromUser(user);
+  };
+
+  const closeModal = () => {
+    setAssign(null);
+    setReturnBookFromUser(null);
+  };
+
   return (
     <section className="p-5 bg-[#DDD] border border-[#C75D2C] bg-opacity-90 max-h-[65vh] ">
       <div className="grid grid-cols-4 gap-6 items-center h-[100%] overflow-y-scroll overflow-x-hidden">
@@ -27,9 +44,12 @@ const Users = (props) => {
           users.map((user) => (
             <UserCard
               cardStyle="bg-[#C75D2C] rounded-md pr-8 pl-2 py-4 text-white w-[250px] h-[175px]"
-              book={user}
+              user={user}
+              assignBook={assignBook}
+              returnBook={returnBook}
             />
           ))}
+        {assign && <UserAssignModal closeModal={closeModal} user={assign} />}
       </div>
     </section>
   );
